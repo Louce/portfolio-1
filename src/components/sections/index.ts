8749798cdaf9d4ff925443aa56d390bf732d5e87
Non Sense <rivaldydendy459@gmail.com>
2025-06-17 11:51:13 +0000

    can you add a section for me to to upload a news, add make an account in

--- src/components/sections/index.ts
+++ src/components/sections/index.ts
@@ -1,5 +1,7 @@
+
 export { Hero } from './Hero';
 export { About } from './About';
 export { Skills } from './Skills';
 export { Projects } from './Projects';
 export { Contact } from './Contact';
+export { News } from './News';
