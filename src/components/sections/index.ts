d2533215aa56ab68ebc63f0137ff757e2824c74c
Non Sense <rivaldydendy459@gmail.com>
2025-06-17 11:56:04 +0000

    instead of news can you make it as feedback and save the feedback in tex

--- src/components/sections/index.ts
+++ src/components/sections/index.ts
@@ -4,4 +4,6 @@ export { About } from './About';
 export { Skills } from './Skills';
 export { Projects } from './Projects';
 export { Contact } from './Contact';
-export { News } from './News';
+export { Feedback } from './Feedback';
+
+    
