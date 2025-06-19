d7b88eb873c1fb47c8d29e19f3c7c81b75be2107
Non Sense <rivaldydendy459@gmail.com>
2025-06-19 01:01:18 +0000

    docs: Add JSDoc documentation

--- src/hooks/use-toast.ts
+++
@@ -1,3 +0,0 @@
-// This file is intentionally left blank for deletion.
-// The custom useToast hook has been deprecated and removed.
-// Components should now import `useToast` from "@/components/ui/use-toast";
