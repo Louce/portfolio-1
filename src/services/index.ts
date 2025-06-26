6116f1b2699f91ef05f60de930fa11e3d67141e5
Non Sense <rivaldydendy459@gmail.com>
2025-06-26 06:30:04 +0000

    docs: Add JSDoc documentation

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,8 +1,6 @@
-import * as feedbackService from './feedbackService';
-
 /**
  * @fileoverview Barrel file for exporting all service modules.
  * This allows for cleaner imports in other parts of the application.
  * e.g., `import { feedbackService } from '@/services';`
  */
-export { feedbackService };
+export * from './feedbackService';
